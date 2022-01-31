0793b6631adca36702b2df4dc1dc83c09149c62e
dmitriyeginov <DylanMr@yandex.ru>
2022-02-01 01:11:15 +0300

    data in char

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -26,6 +26,34 @@ export default () => {
                 <div style={{color: getHouseColor(data.house)}} className="character__house">
                     {data.house}
                 </div>
+                <div>
+                    Дата рождения - {data.dateOfBirth}
+                </div>
+                {
+                    (data.staff || data.student) && 
+                    <div>
+                        {data.staff 
+                            ? <div>Преподаватель</div> 
+                            : (data.student 
+                                ? <div>Студент</div> 
+                                : "")} 
+                    </div>
+                }
+                <div>
+                    {data.wizard 
+                        ? <div>Волшебник</div> 
+                        : <div>Маггл</div>
+                    }
+                </div>
+                <div>
+                    {data.alive
+                        ? <div style={{color: "green"}}>Жив</div>
+                        : <div style={{color: "red"}}>Мертв</div>
+                    }
+                </div>
+                <div>
+                    Патронус - {data.patronus ? data.patronus : "Неизвестно"}
+                </div>
             </div>
         </div>
     )
